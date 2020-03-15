b37b9505f5d15ead7c4dd4aa92ab7185d8fbd0c7
Rory ELLIS <roryellis@Rorys-Air.home>
2020-03-14 23:38:36 -0500

    Added card match check and alert messaging to main.js

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,18 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
