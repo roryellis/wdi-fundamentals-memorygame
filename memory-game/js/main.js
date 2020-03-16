8fe8513ea2983e94c95dc5ed85df571ee2b7a348
Rory ELLIS <roryellis@Rorys-Air.home>
2020-03-15 21:56:12 -0500

    Added reset button and scorecard

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -21,22 +21,25 @@ let cards = [
   }
 ];
 let cardsInPlay = [];
-
-
+let wins = 0;
+let losses = 0;
+let winCount = document.getElementById('wins');
+let lossCount = document.getElementById('losses');
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
+    wins += 1;
+    updateScorecard();
   } else {
     alert("Sorry, try again.");
+    losses +=1;
+    updateScorecard();
   }
 };
 function flipCard() {
   let cardId = this.getAttribute('data-id');
-  console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
   this.setAttribute('src', cards[cardId].cardImage);
 
   if (cardsInPlay.length === 2) {
@@ -53,4 +56,22 @@ function createBoard() {
     document.getElementById('game-board').appendChild(cardElement);
   }
 };
+
+function clearBoard() {
+  document.getElementById('game-board').innerHTML = null;
+}
+
+function updateScorecard() {
+  winCount.textContent = wins;
+  lossCount.textContent = losses;
+}
+
+function resetGame(){
+  clearBoard();
+  createBoard();
+  cardsInPlay = [];
+}
+let resetButton = document.getElementsByTagName('button')[0];
+resetButton.addEventListener('click', resetGame);
+
 createBoard();
